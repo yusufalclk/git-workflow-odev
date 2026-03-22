e7f1501d3b73c606178bd4033176707dee24928e
yusufalclk <alcelikkyusuf@gmail.com>
2026-03-23 01:10:04 +0300

    Register ve login backend altyapısı eklendı arkadaslar!!

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "tictactoe_db";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Veritabanı bağlantı hatası: " . $e->getMessage());
+}
+?>
